3a6e2ac0e18afea92b14041d0c14f547bb907b25
Qronix <jonhead7240@gmail.com>
2018-02-06 13:41:30 -0500

    Commit before UI tweaks.

--- src/includes/sidebar.php
+++ src/includes/sidebar.php
@@ -1,6 +1,6 @@
-<div class="container col-md-2 clearfix" id="sideNav">
+<div class="container col-md-1 clearfix" id="sideNav">
 <ul id="sideNavLinks">
     <li class="sideNavLink"><i class="fa fa-users sideNavIcon" aria-hidden="true"></i><a href="registeruser.php">Users</a></li>
-    <li class="sideNavLink"><i class="fa fa-mobile sideNavIcon largerIcon" aria-hidden="true"></i><a href="phonelist.php">Phone log</a></li>
+    <li class="sideNavLink"><i class="fa fa-mobile sideNavIcon largerIcon" aria-hidden="true"></i><a href="phonelist.php">Phone Inventory</a></li>
 </ul>
 </div>
