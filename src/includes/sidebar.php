285a5115150e0a21737642ae9ea1349859f50130
Qronix <jonhead7240@gmail.com>
2018-02-10 16:40:34 -0500

    Tweak sidebar layout.

--- src/includes/sidebar.php
+++ src/includes/sidebar.php
@@ -1,4 +1,4 @@
-<div class="container col-md-2 clearfix" id="sideNav">
+<div class="container-fluid col-md-2 clearfix" id="sideNav">
 <ul id="sideNavLinks">
     <li class="sideNavLink"><i class="fa fa-users sideNavIcon" aria-hidden="true"></i><a href="registeruser.php">Users</a></li>
     <li class="sideNavLink"><i class="fa fa-mobile sideNavIcon largerIcon" aria-hidden="true"></i><a href="phonelist.php">Phone Inventory</a></li>
